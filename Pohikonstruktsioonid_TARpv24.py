b6d9fda25f39de4a2dbb9196c86c64976a38567d
opilane <opilane@HP-CZC2349HV1>
2025-01-20 11:27:17 +0200

    print() input() funktsioonid 20.01.25

--- Pohikonstruktsioonid_TARpv24.py
+++ Pohikonstruktsioonid_TARpv24.py
@@ -1,2 +1,14 @@
-
-print("Hello World!1", "Hello World!2") 
+print("Hello World")
+#andmetüübid täisarv -int, ujukomaarv -float, sõne -str, tõevärtused - bool 
+#astendamine
+a=5
+aste=2
+tulemus=a**aste
+print(a,"**",aste,"=",tulemus)
+#jagamine /
+arv1=int(input("Esimine korrutaja: "))
+#esimine korrutaja  - первый множитель 
+arv2=int(input("Teine korrutaja: "))
+tulemus=arv1-arv2
+print(arv1,"-",arv2,"=",tulemus)
+print(type(tulemus))
