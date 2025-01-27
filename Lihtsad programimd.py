acb61059e909b88bf35a2d9a24d0abe477e48124
Nikita Orlenko <nikita.orlenko234@gmail.com>
2025-01-27 13:54:41 +0200

    lol

---
+++ Lihtsad programimd.py
@@ -0,0 +1,112 @@
+
+
+
+# Ülesanne 2
+# from math import  *
+# a=3 + 8 / (4 - 2) * 4
+# b=3 + 8 / 4 (- 2 * 4)
+# c=(3 + 8 ) / - 2 * 4 
+# d= 3 + (8 / 4) - 2 * 4
+
+# Ülesanne 3
+# from math import  *
+# try:
+#     R=float(input("R: "))
+#     Srudu=(2*R)**2
+#     Sringi=(pi*R)**2
+#     Pruudu=8*R
+#     Pringi=2*pi*R
+#     print(f"Ruutu pindala: {Srudu}")
+#     print(f"Ringi pindala: {Sringi}")
+#     print(f"Ruudu ümbermõõt: {Pruudu}")
+#     print(f"Ümbermõõdu pikkus: {Pringi}")
+# except:
+#     print("Sisesta ujukomaarvud!")
+
+#     Ülesanne #5
+# kill-koll kill-koll
+# killadi-koll
+# kill-koll kill-koll
+# killadi-koll
+# killadi-koll killadi-koll killadi-koll killadi-koll
+# a="Kill-koll" .capitalize()
+# b="killadi-koll".capitalize()
+# print(a*2,b,a*2,b,a*4)
+
+#Ülesanne 6
+# Rong see sõitis tsuhh tsuhh tsuhh,
+# piilupart oli rongijuht.
+# Rattad tegid rat tat taa,
+# rat tat taa ja tat tat taa.
+# Aga seal rongi peal,
+# kas sa tead, kes olid seal? 
+# a="Rong see sõitis tsuhh tsuhh tsuhh,".capitalize()
+# b="piilupart oli rongijuht.".capitalize()
+# c="Rattad tegid rat tat taa,".capitalize()
+# d="rat tat taa ja tat tat taa.".capitalize()
+# e="Aga seal rongi peal,".capitalize()
+# f="kas sa tead, kes olid seal?".capitalize()
+
+# print(a)
+
+# Ülesanne 7 
+# try:
+#     a=float(input("A: "))
+#     b=float(input("B: "))
+#     if: a>0 b>0
+#     S=a*b
+#     P=(a+b)*2
+#     print(f"S={S}, P={P}")
+#   else:
+# except:
+#     print("Vale andmeid!")
+         
+
+#Ülesanne 8
+# try:
+#     liters=float(input("Kütuse kogus"))
+#     km=float(input("läbitud kilomeetrid"))
+#     Vas=(liters/km)*100
+#     print(f"Keskmine tarbimine on {Vas}")
+# except:
+#     print("Vale andmeid!")
+    
+# Ülesanne 9 
+# from random import randint
+
+
+# try:
+#     print("Rulluisutaja keskmine kiirus on 29,9km/h")
+#     print(" Kui kaugele jõuab M minutiga")
+#     keskkiirus=29.9
+#     M=randint(1,60)
+#     if M>0:
+#         kiirus=29.9*M
+#     else:
+#         print("Vale number ")
+#     print(f"{M} minuti pärast on ta läbi {kiirus} m")
+# except:
+#     print("viga")
+
+try:
+    aeg_min = int(input("Palun sisestage aeg minutites: "))
+    if aeg_min>0:
+        aeg_min/60 
+        aeg_min%
+    else:
+
+except:
+
+    
+
+
+
+    
+   
+    
+            
+
+
+    
+   
+
