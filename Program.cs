a5b8cc90976f8f943fea0655cce2d621984263d8
NikitaORL <nikita.orlenko234@gmail.com>
2025-01-31 16:48:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//programm küsib nime
+//kui sisestasid nime, siis konsool vastab
+//TERE, sinu nimi
+//peab kasutama if ja else
+//kui nime ei sisetata, siis tuleb vastuseks
+// ERROR ja konsool teeb Beep kolm korda 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Palun sisesta oma nimi:");
+        string nimi = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(nimi))  // Kontrollime, kas nimi on sisestatud
+        {
+            Console.Beep();  // Teeb ühe beep'i
+            Console.Beep();  // Teeb teise beep'i
+            Console.Beep();  // Teeb kolmanda beep'i
+            Console.WriteLine("ERROR");
+        }
+        else
+        {
+            Console.WriteLine($"TERE, sinu nimi on {nimi}");
+        }
+    }
+}
