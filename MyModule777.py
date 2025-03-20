4e217acd02df7c2ad5b18b362991a7011c6217ae
opilane <nikita.orlenko234@gmail.com>
2025-03-20 15:38:51 +0200

    Registreerimine ja autoriseerimine"

--- MyModule777.py
+++ MyModule777.py
@@ -41,4 +41,4 @@ def recover_password():
     if username in usernames:
         print(f"Ваш пароль: {passwords[usernames.index(username)]}")
     else:
-        print("Пользователь не найден.")
+        print("Пользователь не найден.")g
